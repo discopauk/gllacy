04dc369e626379931e9b9561bd97e769114cbcca
discopauk <diskopauk@yandex.ru>
2021-09-16 18:42:34 +0300

    more animation

--- src/js/open.js
+++ src/js/open.js
@@ -5,12 +5,9 @@ function openModal(elem) {
 	let target = document.querySelector(`.${elem}`),
 		targetWrap = document.querySelector(`.${elem}__wrap`);
 	target.addEventListener("click", () => {
+		targetWrap.classList.remove("closeModal");
 		targetWrap.classList.toggle("open");
 		target.classList.toggle("open");
-	});
-	document.querySelector(".close").addEventListener("click", () => {
-		targetWrap.classList.remove("open");
-		targetWrap.classList.add("closeModal");
 	});
 }
 
--- src/js/request.js
+++ src/js/request.js
@@ -2,3 +2,7 @@ import { openModal } from "./open";
 
 let modals = ["request"];
 modals.forEach(openModal);
+document.querySelector(".close").addEventListener("click", () => {
+	document.querySelector(".request__wrap").classList.remove("open");
+	document.querySelector(".request__wrap").classList.add("closeModal");
+});
